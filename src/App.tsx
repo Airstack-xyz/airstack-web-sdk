76640e8536995985f6c6fcfebb69c5235c95cf3d
Aadil Hasan <aadil@airstack.xyz>
2023-10-10 18:35:49 +0530

    remove app file changes

--- src/App.tsx
+++ src/App.tsx
@@ -46,7 +46,7 @@ function WithPagginationNoCaching() {
     data,
     loading,
     pagination: { hasNextPage, hasPrevPage, getNextPage, getPrevPage },
-  } = useQueryWithPagination<{ name: "test" }>(query, variables, {
+  } = useQueryWithPagination(query, variables, {
     cache: false,
   });
 
@@ -66,16 +66,8 @@ function WithPagginationNoCaching() {
     </div>
   );
 }
-type Vars = typeof variables;
 function LazyLoad() {
-  const [fetch, { data, loading }] = useLazyQuery(query, variables, {
-    cache: false,
-    dataFormatter: (data) => {
-      return data.user.data as {
-        name: string;
-      };
-    },
-  });
+  const [fetch, { data, loading }] = useLazyQuery(query, variables);
 
   return (
     <div>
