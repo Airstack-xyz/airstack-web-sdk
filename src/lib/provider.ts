5aaa6e7d7d2f20cca42210eb699f16129107ea56
Aadil Hasan <aadil@airstack.xyz>
2023-12-07 10:55:36 +0530

    fix no api key provided error, when using provider

--- src/lib/provider.ts
+++ src/lib/provider.ts
@@ -1,17 +1,18 @@
-import { useEffect } from "react";
 import { Config } from "./config";
 import { init } from "./init";
 
 type ProviderProps = {
-    apiKey: string;
-    children: JSX.Element;
+  apiKey: string;
+  children: JSX.Element;
 } & Omit<Config, "authKey">;
 
-export function AirstackProvider({ children, apiKey, ...config }: ProviderProps) {
-
-    useEffect(() => {
-        init(apiKey, config)
-    }, [apiKey, config])
-
-    return children
+export function AirstackProvider({
+  children,
+  apiKey,
+  ...config
+}: ProviderProps) {
+  // don't call init inside an effect as the child components/hooks effects will get called before this one
+  // and the child components/hooks will not have access to the apiKey
+  init(apiKey, config);
+  return children;
 }
