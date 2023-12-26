efb93759db65ebeaa26312e7aec057246e8b854e
kaushalmeena <kaushal.meena1996@gmail.com>
2023-12-26 13:04:40 +0530

    add polyfill for buffer in vite.static.config

--- vite.static.config.ts
+++ vite.static.config.ts
@@ -1,9 +1,14 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(), 
+    // we need buffer polyfill for @xmtp/xmtp-js
+    nodePolyfills({ include: ["buffer"] })
+  ],
   build: {
     outDir: "dist-static",
   },
